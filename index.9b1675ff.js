582a6b63c47c5fc28fb8ec579b7c0591c8f2296e
Alex Rog <sasharg303@gmail.com>
2023-11-28 10:59:36 +0100

    make build

---
+++ index.9b1675ff.js
@@ -0,0 +1,3 @@
+var e=document.querySelector("#thumbs"),t=document.querySelector("#largeImg");e.addEventListener("click",function(e){e.preventDefault();var r=e.target.closest(".list-item");if(r){var c=r.querySelector(".list-item__link").href;t.src=c}});//# sourceMappingURL=index.9b1675ff.js.map
+
+//# sourceMappingURL=index.9b1675ff.js.map
